8079b7a3602cb74e16c5334e62df06ccd843c4c3
Amir Ubay <amir.ubaidillah@gmail.com>
2023-02-12 17:16:09 +0700

    Update web menu nav

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
    return (
       <>
          <div className="navbar bg-transparent">
-            <a href="/" className="btn btn-ghost normal-case text-xl">QA Sandbox</a>
+            <a href="/" className="btn btn-ghost normal-case text-xl">Sandbox QA</a>
          </div>
          <div className="divide rm-0 pl-4 pr-4 bg-transparent"></div>
       </>
